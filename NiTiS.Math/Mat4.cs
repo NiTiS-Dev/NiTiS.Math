edf46ae3e29b818cdc9b49d1236a567126a44ff2
NiTiSon <NiTiSon@users.noreply.github.com>
2023-02-06 22:13:17 +0300

    Imprv

--- NiTiS.Math/Mat4.cs
+++ NiTiS.Math/Mat4.cs
@@ -1,36 +1,90 @@
-﻿using System.Numerics;
+﻿using System;
+using System.Globalization;
+using System.Numerics;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using static System.Runtime.CompilerServices.MethodImplOptions;
 
 namespace NiTiS.Math;
 
-public readonly unsafe struct Mat4<T>
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public unsafe struct Mat4<T>
 	where T : unmanaged, INumberBase<T>
 {
 	#region Matrix
-	public readonly T M11;
-	public readonly T M12;
-	public readonly T M13;
-	public readonly T M14;
+	public const int
+		RowsCount = 4,
+		ColumnsCount = 4,
+		ElementCount = ColumnsCount * RowsCount;
 
-	public readonly T M21;
-	public readonly T M22;
-	public readonly T M23;
-	public readonly T M24;
+	public T M11;
+	public T M12;
+	public T M13;
+	public T M14;
 
-	public readonly T M31;
-	public readonly T M32;
-	public readonly T M33;
-	public readonly T M34;
+	public T M21;
+	public T M22;
+	public T M23;
+	public T M24;
 
-	public readonly T M41;
-	public readonly T M42;
-	public readonly T M43;
-	public readonly T M44;
+	public T M31;
+	public T M32;
+	public T M33;
+	public T M34;
+
+	public T M41;
+	public T M42;
+	public T M43;
+	public T M44;
 	#endregion
+
+	public Vector4D<T> Row1
+		=> Unsafe.As<T, Vector4D<T>>(ref M11);
+	public Vector4D<T> Row2
+		=> Unsafe.As<T, Vector4D<T>>(ref M21);
+	public Vector4D<T> Row3
+		=> Unsafe.As<T, Vector4D<T>>(ref M31);
+	public Vector4D<T> Row4
+		=> Unsafe.As<T, Vector4D<T>>(ref M41);
+
+	public Vector4D<T> Column1
+		=> new(M11, M21, M31, M41);
+	public Vector4D<T> Column2
+		=> new(M12, M22, M32, M42);
+	public Vector4D<T> Column3
+		=> new(M13, M23, M33, M43);
+	public Vector4D<T> Column4
+		=> new(M14, M24, M34, M44);
+	
+	public T this[int index]
+	{
+		get
+		{
+			if (index >= ElementCount)
+				throw new ArgumentOutOfRangeException(nameof(index));
+
+			return Unsafe.Add(ref M11, index);
+		}
+	}
+	public T this[int x_, int _x]
+	{
+		get
+		{
+			if (_x >= RowsCount)
+				throw new ArgumentOutOfRangeException(nameof(_x));
+
+			if (x_ >= ColumnsCount)
+				throw new ArgumentOutOfRangeException(nameof(x_));
+
+			return Unsafe.Add(ref M11, x_ * 4 + _x);
+		}
+	}
+
+
 	public Vector3D<T> Translation
 		=> new(M41, M42, M43);
-	public static Mat4<T> Identity => new(
+
+	public static readonly Mat4<T> Identity = new(
 		T.One, T.Zero, T.Zero, T.Zero,
 		T.Zero, T.One, T.Zero, T.Zero,
 		T.Zero, T.Zero, T.One, T.Zero,
@@ -65,17 +119,43 @@ public readonly unsafe struct Mat4<T>
 
 
 	/// <summary>Indicates whether the current matrix is the identity matrix.</summary>
-	/// <value><see langword="true" /> if the current matrix is the identity matrix; otherwise, <see langword="false" />.</value>
+	/// <value><see langword="true" /> if the current matrix is the identity matrix; otherwise: <see langword="false" />.</value>
 	public readonly bool IsIdentity
+			=> M11 == T.One  && M22 == T.One  && M33 == T.One && M44 == T.One
+			&& M12 == T.Zero && M13 == T.Zero && M14 == T.Zero
+			&& M21 == T.Zero && M23 == T.Zero && M24 == T.Zero
+			&& M31 == T.Zero && M32 == T.Zero && M34 == T.Zero
+			&& M41 == T.Zero && M42 == T.Zero && M43 == T.Zero;
+	public override readonly int GetHashCode()
 	{
-		get
-		{
-			return
-				M11 == T.One  && M22 == T.One  && M33 == T.One  && M44 == T.One &&
-				M12 == T.Zero && M13 == T.Zero && M14 == T.Zero &&
-				M21 == T.Zero && M23 == T.Zero && M24 == T.Zero &&
-				M31 == T.Zero && M32 == T.Zero && M34 == T.Zero &&
-				M41 == T.Zero && M42 == T.Zero && M43 == T.Zero;
-		}
+		HashCode hash = default;
+
+		hash.Add(M11);
+		hash.Add(M12);
+		hash.Add(M13);
+		hash.Add(M14);
+
+		hash.Add(M21);
+		hash.Add(M22);
+		hash.Add(M23);
+		hash.Add(M24);
+
+		hash.Add(M31);
+		hash.Add(M32);
+		hash.Add(M33);
+		hash.Add(M34);
+
+		hash.Add(M41);
+		hash.Add(M42);
+		hash.Add(M43);
+		hash.Add(M44);
+
+		return hash.ToHashCode();
 	}
+	public override readonly string ToString()
+		=> string.Format(CultureInfo.CurrentCulture, "{{ {{M11:{0} M12:{1} M13:{2} M14:{3}}} {{M21:{4} M22:{5} M23:{6} M24:{7}}} {{M31:{8} M32:{9} M33:{10} M34:{11}}} {{M41:{12} M42:{13} M43:{14} M44:{15}}} }}",
+							 M11, M12, M13, M14,
+							 M21, M22, M23, M24,
+							 M31, M32, M33, M34,
+							 M41, M42, M43, M44);
 }
---
+++ Tests/Matrix4x4Tests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NiTiS.Math.Tests;
+
+public class Matrix4x4Tests
+{
+	[SetUp]
+	public void Setup()
+	{
+
+	}
+	[Test]
+	public void Tesst()
+	{
+		Mat4<float> mat4 = Mat4<float>.Identity;
+
+		mat4.M11 = 1f;
+
+	}
+}
