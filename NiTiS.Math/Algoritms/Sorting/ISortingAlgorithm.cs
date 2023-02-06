008fb7c697f02d7be14dbc39e7d3f3cff08144a1
NiTiSon <NiTiSon@users.noreply.github.com>
2023-02-06 21:20:56 +0300

    + Sorting

---
+++ NiTiS.Math/Algoritms/Sorting/ISortingAlgorithm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using static System.Runtime.CompilerServices.MethodImplOptions;
+
+namespace NiTiS.Math.Algoritms.Sorting;
+
+public interface ISortingAlgorithm<T>
+{
+    [MethodImpl(AggressiveInlining | AggressiveOptimization)]
+    protected static void Swap(Span<T> buff, int x, int y)
+        => (buff[y], buff[x]) = (buff[x], buff[y]);
+
+    [MethodImpl(AggressiveInlining | AggressiveOptimization)]
+    protected static void Swap(T[] buff, int x, int y)
+        => (buff[y], buff[x]) = (buff[x], buff[y]);
+}
---
+++ Tests/BubbleSortingTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using NiTiS.Math.Algoritms.Sorting;
+
+namespace NiTiS.Math.Tests;
+
+public class BubbleSortingTests
+{
+	[SetUp]
+	public void Setup()
+	{
+
+	}
+	[Test]
+	public void BubbleSorting()
+	{
+		Span<int> span = stackalloc int[] { 5, 1, 9 };
+		Span<int> req = stackalloc int[] { 1, 5, 9 };
+
+		BubbleSorting<int>.Sort(span);
+		Assert.IsTrue(req[0] == span[0]);
+		Assert.IsTrue(req[1] == span[1]);
+		Assert.IsTrue(req[2] == span[2]);
+	}
+	[Test]
+	public void BackBubbleSorting()
+	{
+		Span<int> span = stackalloc int[] { 5, 1, 9, 4 };
+		Span<int> req = stackalloc int[] { 9, 5, 4, 1 };
+
+		BubbleSorting<int>.BackSort(span);
+		Assert.IsTrue(req[0] == span[0]);
+		Assert.IsTrue(req[1] == span[1]);
+		Assert.IsTrue(req[2] == span[2]);
+		Assert.IsTrue(req[3] == span[3]);
+		Assert.IsTrue(req[3] == span[3]);
+	}
+}
