9310f4057babf4364dd2ddab964fd275981cd249
NiTiSon <NiTiSon@users.noreply.github.com>
2023-02-08 23:58:30 +0300

    filenest

--- Tests/Matrix4x4Tests.cs
+++ Tests/Matrix4x4Tests.cs
@@ -7,13 +7,17 @@ public class Matrix4x4Tests
 	[SetUp]
 	public void Setup()
 	{
+
 	}
 	[Test]
 	public void Tesst()
 	{
 		Matrix4x4<float> mat4 = Matrix4x4<float>.Identity;
 
+		//System.Numerics.Matrix4x4.CreateConstrainedBillboard()
+
 		mat4.M11 = 1f;
 
+		System.Console.WriteLine(mat4);
 	}
 }
